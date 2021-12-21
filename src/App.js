ce4949c7ad0a4a2c560691ec6c7076ab615c363c
feeduraddiction <alfanex62@gmail.com>
2021-12-22 00:01:31 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [currency, setCurrency] = useState([]);
+  const fetchData = async () =>{
+    try {
+      const response = await fetch('http://localhost:3002');
+      const json = await response.json()
+      setCurrency(json);
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+  console.log(currency)
+  useEffect(()=>{
+    fetchData();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {Array.from(currency).map(currency => (
+          <li>
+            {currency.Cur_Abbreviation}
+            {currency.Cur_OfficialRate}
+          </li>
+        ))}
+      </ul>
+      <select>
+      {Array.from(currency).map(currency => (
+          <option>
+            {currency.Cur_Abbreviation}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
